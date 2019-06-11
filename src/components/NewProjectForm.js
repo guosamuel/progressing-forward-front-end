972d92acdcf645aaa7cfb899a588b1b9a86b844b
github username <github email address>
2019-06-11 18:09:09 -0400

    got the new project form info to the back end

--- src/components/NewProjectForm.js
+++ src/components/NewProjectForm.js
@@ -1,31 +1,90 @@
 import React, { Component } from 'react'
+// import DateForm from './DateForm'
 import { DateInput } from 'semantic-ui-calendar-react'
+import { connect } from 'react-redux'
 
 class NewProjectForm extends Component {
 
+  state = {
+    title: "",
+    description: "",
+    date: "",
+    user_id: this.props.current_user.id
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleDateChange = (event, {name, value}) => {
+    // event.persist()
+    // console.log(event, name, value)
+    // debugger
+    this.setState({[name]: value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    fetch('http://localhost:3000/api/v1/projects', {
+      method: "POST",
+      headers: {
+        "Content-Type": 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(resp => resp.json())
+    .then(newProject => console.log("I'M IN THE HANDLE EVENT", newProject))
+  }
+
   render() {
+    console.log("I AM IN THE NEW PROJECT", this.props)
     return (
-      <form class="ui form">
-        <div class="field">
+      <form className="ui form" onSubmit={this.handleSubmit}>
+        <div className="field">
           <label>Title</label>
-          <input type="text" name="title" placeholder="Title" />
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
         </div>
-        <div class="field">
+        <div className="field">
           <label>Description</label>
-          <textarea rows="4" cols="50" name="description" placeholder="Enter description here" />
+          <textarea
+            rows="4"
+            cols="50"
+            name="description"
+            placeholder="Enter description here"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
         </div>
-        <div class="field>">
+        <div className="field">
           <label>Due Date</label>
           <DateInput
+            name="date"
             placeholder="Date"
-            name='date'
+            value={this.state.date}
+            iconPosition="left"
+            onChange={this.handleDateChange}
+            type="text"
+            dateFormat="MM/DD/YYYY"
+            minDate={new Date()}
           />
         </div>
         <br />
-        <button class="ui button" type="submit">Submit</button>
+        <button className="ui button" type="submit">Submit</button>
       </form>
     )
   }
 }
 
-export default NewProjectForm
+const mapStateToProps = state => {
+  return {
+    current_user: state.usersReducer.current_user
+  }
+}
+export default connect(mapStateToProps)(NewProjectForm)
