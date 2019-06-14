88628f78db5e49febf59a6151676372996508a86
github username <github email address>
2019-06-13 20:01:42 -0400

    Turned project lead and collaborators into state and rendered collaborator button

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,11 +6,29 @@ import { connect } from 'react-redux'
 import NewTaskForm from './NewTaskForm'
 
 class Project extends Component {
+  constructor(props) {
+    super(props)
 
-  state = {
-    tasksShown: false,
-    taskFormShown: false
+    const projectLead = this.props.allUsers.find( user => user.id === this.props.project.project_lead_id)
+    const collaborators = this.props.project.users.filter( user => user.id !== projectLead.id )
+
+    this.state = {
+      tasksShown: false,
+      taskFormShown: false,
+      moreCollaboratorsShown: false,
+      collaborators: collaborators,
+      projectLead: projectLead
+    }
+
   }
+
+  // componentDidMount() {
+  //   debugger
+  //   this.setState({
+  //     projectLead: projectLead,
+  //     collaborators: [...collaborators]
+  //   })
+  // }
 
   displayTasks = () => {
     this.setState({tasksShown: !this.state.tasksShown})
@@ -21,6 +39,10 @@ class Project extends Component {
     this.setState({taskFormShown: !this.state.taskFormShown})
   }
 
+  displayMoreCollaborators = () => {
+    this.setState({moreCollaboratorsShown: !this.state.moreCollaboratorsShown})
+  }
+
   // findProjectLead = () => {
   //   return projectLead
   // }
@@ -29,8 +51,9 @@ class Project extends Component {
     // console.log("IM IN THE PROJECT COMPONENT", this.props)
     const filteredTasks = this.props.allTasks.filter( task => task.project_id === this.props.project.id)
     const renderTasks = filteredTasks.map( task => <Task task={task} key={task.id} projectDueDate={this.props.project.due_date}/> )
-    const projectLead = this.props.allUsers.find( user => user.id === this.props.project.project_lead_id)
-    const allCollaborators = this.props.project.users.filter( user => user.id !== projectLead.id ).map( collaborator => {
+    // const projectLead = this.props.allUsers.find( user => user.id === this.props.project.project_lead_id)
+    // const allCollaborators = this.props.project.users.filter( user => user.id !== projectLead.id )
+    const renderCollaborators = this.state.collaborators.map( collaborator => {
       return (
         <div className="item">
           <i className="right triangle icon"></i>
@@ -54,13 +77,22 @@ class Project extends Component {
             <br/>
             Description: {this.props.project.description}
             <br/>
-            Project Lead: {projectLead.first_name} {projectLead.last_name}
+            Project Lead: { this.state.projectLead ? `${this.state.projectLead.first_name} ${this.state.projectLead.last_name}` : "TBD" }
             <br />
             Project Due Date: {sanitizeDate(this.props.project.due_date)}
             <br />
+            <br />
             Collaborators:
+            <br />
+            <br />
+            <div>
+              <button className="compact ui icon button" onClick={this.displayMoreCollaborators}>
+                { this.state.moreCollaboratorsShown ? <i className="down chevron icon"></i> : <i className="right chevron icon"></i> }
+              Add Collaborator(s)
+              </button>
+            </div>
             <div className="ui list">
-              { allCollaborators }
+              { renderCollaborators }
             </div>
           </div>
           <br />
